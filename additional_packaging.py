f4177f2b25929c0c4ed9eb1a2e903c55303b13ca
Alessandro Serpi <alessandro.serpi@me.com>
2025-04-04 00:39:58 +0200

    fix: correct output folder in additional packaging

--- additional_packaging.py
+++ additional_packaging.py
@@ -3,12 +3,12 @@ import shutil
 
 
 def additional_packaging(ta_name):
-    output_path = pathlib.Path("output")
+    output_path = pathlib.Path("output") / ta_name
 
     # Copy README
     shutil.copy(pathlib.Path("README.md"), output_path)
 
     # Copy license
-    licenses_path = output_path / ta_name / "LICENSES"
+    licenses_path = output_path / "LICENSES"
     licenses_path.mkdir(parents=True, exist_ok=True)
     shutil.copy(pathlib.Path("LICENSE.txt"), licenses_path)
